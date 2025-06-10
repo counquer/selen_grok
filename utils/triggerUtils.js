4ee7f4a32b15479571e74d768622fd28a8898b7b
counquer <counquer@gmail.com>
2025-06-09 23:32:26 -0400

    Update triggerUtils.js

--- utils/triggerUtils.js
+++ utils/triggerUtils.js
@@ -1,11 +1,16 @@
+const path = require('path');
 const { normalize } = require(path.resolve(__dirname, '../../utils/triggerUtils.js'));
-const Client = require("@notionhq/client").Client;
+const { Client } = require("@notionhq/client");
 const dotenv = require("dotenv");
+const logger = require('../../utils/logger.js');
+
+dotenv.config(); // Solo una vez
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 const DB_TRIGGERS = process.env.DB_TRIGGERS;
 const DB_MEMORIA_CURADA = process.env.DB_MEMORIA_CURADA;
+
 
 /**
  * Limpia texto: remueve caracteres invisibles y codifica como UTF-8 seguro.
